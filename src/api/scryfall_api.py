0cf73036cf1703d6607404556bde2f53b67d03af
Tomanta <bzednick@gmail.com>
2025-01-15 21:24:33 -0600

    Testing API calls, can pull bulk data now.

--- src/api/scryfall_api.py
+++ src/api/scryfall_api.py
@@ -1,2 +1,29 @@
-scryfall_root = "https://api.scryfall.com"
-scryfall_wait_time = 50 # ms
+import requests
+from ratelimit import limits
+
+SCRYFALL_ROOT = "https://api.scryfall.com"
+SCRYFALL_HEADERS = {
+    "User-Agent": "MTG_Wishlist/1.0",
+    "Accept": "*/*",
+}
+
+SCRYFALL_ENDPOINTS = {"bulk": "/bulk-data"}
+
+ONE_SECOND = 1  # Scryfall requests only sending 10 request per second; we'll be nice and say 1 per second
+
+
+@limits(calls=1, period=ONE_SECOND)
+def call_api(endpoint):
+    if endpoint not in SCRYFALL_ENDPOINTS:
+        raise ValueError()
+
+    url = f"{SCRYFALL_ROOT}{SCRYFALL_ENDPOINTS[endpoint]}"
+    response = requests.get(url, SCRYFALL_HEADERS)
+    if response.status_code == 429:
+        raise Exception("429: Fatal Error, Too many requests!")
+    elif response.status_code == 404:
+        raise Exception("404: Endpoint not found!")
+    elif response.status_code != 200:
+        raise Exception(f"API response {response.status_code}")
+
+    print(response.json()["data"][0]["type"])
--- src/commands.py
+++ src/commands.py
@@ -1,7 +1,9 @@
 import click
+from api.scryfall_api import call_api
 
 
 @click.command()
 def bulk():
     click.echo("Hello!")
+    call_api("bulk")
     SystemExit(0)  # Success
